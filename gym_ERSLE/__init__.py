6bd9e3a3760afba390a8773673076b921be9c0c0
Abhinav Bhatia <bhatiaabhinav93@gmail.com>
2018-01-06 09:50:27 +0800

    10 mins and 15 mins decision interval environments added

--- gym_ERSLE/__init__.py
+++ gym_ERSLE/__init__.py
@@ -1,19 +1,28 @@
 from gym.envs.registration import register
 from gym_ERSLE.pyERSEnv import ToyScene
+from gym_ERSLE.pyERSEnv import Scene4
 
-for decision_interval in [1, 30, 60, 120, 240, 360, 720, 1440]:
-    for ca in [False, True]:
-        for im in [False, True]:
-            for dynamic in [False, True]:
-                register(
-                    id='pyERSEnv{0}{1}{2}{3}-v3'.format(
-                        '-im' if im else '',
-                        '-ca' if ca else '',
-                        '-dynamic' if dynamic else '',
-                        '-{0}'.format(decision_interval) if decision_interval > 1 else ''
-                    ),
-                    entry_point='gym_ERSLE:ToyScene',
-                    kwargs={'discrete_action':not ca, 'discrete_state':not im, 'decision_interval': decision_interval, 'dynamic': dynamic},
-                    max_episode_steps = 10000000,
-                    nondeterministic=False
-                )
+version_to_scene_map = {
+    'v3': 'gym_ERSLE:ToyScene',
+    'v4': 'gym_ERSLE:Scene4'
+}
+
+for version in ['v3', 'v4']:
+    for decision_interval in [1, 10, 15, 30, 60, 120, 240, 360, 720, 1440]:
+        for ca in [False, True]:
+            for im in [False, True]:
+                for dynamic in [False, True]:
+                    register(
+                        id='pyERSEnv{0}{1}{2}{3}-{4}'.format(
+                            '-im' if im else '',
+                            '-ca' if ca else '',
+                            '-dynamic' if dynamic else '',
+                            '-{0}'.format(decision_interval) if decision_interval > 1 else '',
+                            version
+                        ),
+                        entry_point=version_to_scene_map[version],
+                        kwargs={'discrete_action': not ca, 'discrete_state': not im,
+                                'decision_interval': decision_interval, 'dynamic': dynamic},
+                        max_episode_steps=10000000,
+                        nondeterministic=False
+                    )
