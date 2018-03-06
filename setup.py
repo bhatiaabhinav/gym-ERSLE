461725d1142b299ca28a4075b2138a4548e484df
Abhinav Bhatia <bhatiaabhinav93@gmail.com>
2018-03-07 07:24:38 +0800

    updated dependencies in setup.py

--- setup.py
+++ setup.py
@@ -2,5 +2,5 @@ from setuptools import setup
 
 setup(name='gym_ERSLE',
       version='0.0.1',
-      install_requires=['gym', 'gymGame', 'numpy']
-)
+      install_requires=['gym', 'gymGame', 'numpy', 'sklearn']
+      )
